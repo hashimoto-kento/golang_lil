dd4534721e4622b7341a7a82e2f1bba4cfd9d557
DESKTOP-J40HMMS\Owner <bug.bee.ron@gmail.com>
2024-12-08 17:07:00 +0900

    redirect add

--- Todo/main.go
+++ Todo/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"html/template"
-	"log"
 	"net/http"
 )
 
@@ -12,28 +11,31 @@ var todoList []string
 func handleTodo(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/todo.html")
 	if err != nil {
-		log.Printf("Template parsing error: %v", err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 }
 
 err = t.Execute(w, todoList)
 if err != nil {
-	log.Printf("Template executing error: %v", err)
 	http.Error(w, "Internal server error", http.StatusInternalServerError)
 	return
 }
 }
 
-func main() {
-	todoList = append(todoList, "Buy Milk", "Buy Eggs", "Buy Bread")
+func handleAdd(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		todo := r.Form.Get("todo")
+		todoList = append(todoList, todo)
+		http.Redirect(w, r, "/todo", 303)
+}
 
+func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	http.HandleFunc("/todo", handleTodo)
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	http.HandleFunc("/add", handleAdd)
+
+	http.ListenAndServe(":8080", nil)
+
 }
