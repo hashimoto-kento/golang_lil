87d805941595d883794b55d45699c618a70ceaab
DESKTOP-J40HMMS\Owner <bug.bee.ron@gmail.com>
2024-12-31 09:56:14 +0900

    セッション管理実装

--- Todo/main.go
+++ Todo/main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"html/template"
+	// "html/template"
 	"net/http"
 )
 
 var todoList []string
 
 // template engine, blank identifier to ignore error
-func handleTodo(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/todo.html")
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-}
+// func handleTodo(w http.ResponseWriter, r *http.Request) {
+// 	t, err := template.ParseFiles("templates/todo.html")
+// 	if err != nil {
+// 		http.Error(w, "Internal server error", http.StatusInternalServerError)
+// 		return
+// }
 
-err = t.Execute(w, todoList)
-if err != nil {
-	http.Error(w, "Internal server error", http.StatusInternalServerError)
-	return
-}
-}
+// err = t.Execute(w, todoList)
+// if err != nil {
+// 	http.Error(w, "Internal server error", http.StatusInternalServerError)
+// 	return
+// }
+// }
 
-func handleAdd(w http.ResponseWriter, r *http.Request) {
-		r.ParseForm()
-		todo := r.Form.Get("todo")
-		todoList = append(todoList, todo)
-		http.Redirect(w, r, "/todo", 303)
-}
+// func handleAdd(w http.ResponseWriter, r *http.Request) {
+// 		r.ParseForm()
+// 		todo := r.Form.Get("todo")
+// 		todoList = append(todoList, todo)
+// 		http.Redirect(w, r, "/todo", 303)
+// }
 
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
